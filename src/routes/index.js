528147c433b8db8bfff9dd231696f5c85095e5fb
jim <jamie_1019@sina.com>
2016-10-21 19:48:52 +0800

    Build UI template

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,8 @@ router.get('/report', function(req, res, next) {
   res.render('report', { title: 'Express' });
 });
 
+router.get('/mgmConsole', function(req, res, next) {
+  res.render('management/index', { title: 'Express' });
+});
+
 module.exports = router;
