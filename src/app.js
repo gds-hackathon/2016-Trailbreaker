f68d027b0dc589f6371dced5365f076a691e5286
Shaowei Lin <Shaowei Lin>
2016-10-21 11:55:16 -0700

    introduce wechat

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var checkSignature = require('./midware/checkSignature');
+var wechatHandler = require('./midware/wechatHandler');
 
 var routes = require('./routes/index');
 
@@ -31,7 +32,19 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(function(req, res, next){
+   if(appConfig.BASE_URL === ''){
+       appConfig.BASE_URL = 'http://' + req.hostname;
+       if(app.get('port') !=  80){
+          appConfig.BASE_URL += ':' + app.get('port');
+       }
+   } 
+
+   //console.log(appConfig.BASE_URL);
+   next();
+});
 app.use('/pages/', checkSignature);
+app.use('/wechat/gd/api', wechatHandler);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
---
+++ src/midware/wechatHandler.js
@@ -0,0 +1,87 @@
+
+var handler = function (req, res, next) {
+  // message is located in req.weixin
+  var message = req.weixin;
+  console.log('receive message');
+  console.log(req.weixin);
+
+  var signatureHelper = require('../helpers/signatureHelper');
+  
+  if(message.FromUserName == 'otCTjtzgIIpuMfEOU9LJHcPlAi4A'){
+    if(message.MsgType === 'text')
+    {
+        var ts = (Date.now());
+        var nonce = guid() + message.FromUserName.toLowerCase();
+        var signature = signatureHelper.computeSignature(ts, nonce);
+
+        var urlPart = 'ts=' +  + '&nonce=' + nonce + '&signature=' + signature;
+
+        if(/^\s*(register|zhuce|注册)/i.test(message.Content)){ 
+           var url = appConfig.BASE_URL + '/registration' + '?' + urlPart;
+
+            res.reply([
+            {
+              title: 'Discount',
+              description: 'Discount message goes here. Welcome!',
+              picurl: 'http://img13.360buyimg.com/cms/jfs/t3406/29/406493902/87685/b6e50be3/58087f30Nbadb1818.jpg',
+              url: url
+            }
+          ]);
+        }else if(/^\s*(discout|dazhe|打折)/i.test(message.Content)){
+            var url = appConfig.BASE_URL + '/transaction' + '?' + urlPart;
+            res.reply([
+            {
+              title: 'Discount',
+              description: 'Discount message goes here. Welcome!',
+              picurl: 'http://img13.360buyimg.com/cms/jfs/t3406/29/406493902/87685/b6e50be3/58087f30Nbadb1818.jpg',
+              url: url
+            }
+          ]);
+        }
+    }
+    res.reply({
+        type:'text',
+        //content: JSON.stringify(message),
+        content: 'Thank you for subscribe 程序员肖恩! \r\n'
+        + 'Please input "register|zhuce|注册"for register if you are not a user\r\n'
+        + 'Please input "discout|dazhe|打折" for to get discount! :)\r\n'
+    });
+  }
+  /*
+  if (message.FromUserName === 'wizardlsw') {
+    // reply with text
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    // another way to reply with text
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // reply with music
+    res.reply({
+      type: "music",
+      content: {
+        title: "Just some music",
+        description: "I have nothing to lose",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3"
+      }
+    });
+  } else {
+    // reply with thumbnails posts
+    res.reply([
+      {
+        title: 'Come to fetch me',
+        description: 'or you want to play in another way ?',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }*/
+};
+
+
+var wechat = require('wechat');
+
+module.exports  = wechat(appConfig.wechat.wechat_token, handler);
