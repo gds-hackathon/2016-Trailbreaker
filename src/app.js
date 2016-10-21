da122c47ed129140db98193a0f6825a2ec3ace8a
unknown <>
2016-10-21 17:16:28 +0800

    delete routes users

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var employees = require('./routes/admin/employees');
 
