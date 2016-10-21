b25cd7ea1c5197e6976b48deb5ea179ce3019ea0
unknown <>
2016-10-21 20:08:01 +0800

    add transaction page

---
+++ src/public/scripts/transactionController.js
@@ -0,0 +1,3 @@
+function TransactionContorller($scope) {
+    $scope.title = "Transaction";
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,9 +9,10 @@ router.get('/', function(req, res, next) {
 router.get('/report', function(req, res, next) {
   res.render('report', { title: 'Express' });
 });
-
-
 router.get('/registration', function(req, res, next) {
   res.render('registration', { title: 'Customer Registration' });
+});
+router.get('/transaction', function(req, res, next) {
+  res.render('transaction', { title: 'Transaction' });
 });
 module.exports = router;
