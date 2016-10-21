d76cc341c9c4224e04cd9aa3f2ad9dd2137ac56f
jim <jamie_1019@sina.com>
2016-10-21 18:56:41 +0800

    Introduce angular-chart

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,7 +1,2 @@
 
-angular.module('trailbreakerApp', []).config(['$routeProvider', function ($routeProvider) {
-    // $routeProvider.
-    //     when('/index', { templateUrl: '../views/index.html', controller: IndexContorller }).
-    //     when('/report', { templateUrl: '../views/report.html', controller: ReportContorller }).
-    //     otherwise({ redirectTo: '/index' });
-}]);
+angular.module('trailbreakerApp', []);
