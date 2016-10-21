3484f27de3b44ea621a3037b0b822bddfd3ae86f
Shaowei Lin <Shaowei Lin>
2016-10-21 05:22:07 -0700

    /api/vendor

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('./lib/extensions');
 require('./appConfig.js');
 var express = require('express');
 var path = require('path');
@@ -5,11 +6,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var checkSignature = require('./midware/checkSignature');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var employees = require('./routes/admin/employees');
+var vendor = require('./routes/api/vendor');
 //var employees = require('./routes/api/transaction');
 
 var app = express();
@@ -26,6 +29,7 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use('/pages/', checkSignature);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -33,6 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/admin/employees', employees);
+
+app.use('/api/vendor', vendor);
 //app.use('/api/transaction', transaction);
 
 // catch 404 and forward to error handler
