3d296f31fd24c5ea5fb8b39cb01c6f6fd77a4bea
Shaowei Lin <Shaowei Lin>
2016-10-21 17:54:52 -0700

    change image

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(function(req, res, next){
    next();
 });
 // app.use('/pages/', checkSignature);
-app.use('/wechat/gd/api', wechatHandler);
+app.use('/fs/wx/interface', wechatHandler);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
